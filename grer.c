0ab42e4ad832b00cad3e5d1f026330a9b7fed3c1
msssumanth <51699318+msssumanth@users.noreply.github.com>
2019-06-18 14:58:25 +0530

    Create grer.c

---
+++ grer.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	char a;
+	scanf("%c",&a);
+	 if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+	 {
+	 	printf("Vowel");
+	 }
+	 
+	 else
+	 {
+	  if(((a>=65)&&(a<=91))||((a>=97)&&(a<=123)))
+	  {
+		   	 
+	 printf("Consonant");
+}
+	else
+	printf("Invalid");
+}
+
+	return 0;
+}

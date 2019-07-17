9e8c2d779d1f900b21e0d575ed13dd7e6da1fef9
msssumanth <51699318+msssumanth@users.noreply.github.com>
2019-07-17 13:36:36 +0530

    Create 11array.c

---
+++ 11array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10],i,n,b,j,temp=0,count=0;
+    scanf("%d %d",&n,&b);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+   
+    for(i=0;i<n;i++)
+    {
+        if(b==a[i])
+        count+=1;
+       
+    }
+    if(count>=1)
+    {
+        printf("Yes");
+    }
+    else
+    printf("no");
+
+    return 0;
+}
